4b611d5bc928462910ea5d9a7d6d39f998e2beea
Shivam Chouhan <42615134+shivam1597@users.noreply.github.com>
2023-06-22 01:18:09 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,8 +20,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2d55fb17d4dfe47ba4dd7c43f4a2a44e",
-"/": "2d55fb17d4dfe47ba4dd7c43f4a2a44e",
+"index.html": "42b7f6e7aa8937b4c0cd137aef602d9c",
+"/": "42b7f6e7aa8937b4c0cd137aef602d9c",
 "main.dart.js": "dfb9141fdbd32c259896edb45a601526",
 "manifest.json": "ce2b9107c0c4d7891c9ce696a89ddf53",
 "version.json": "5e231e20f33a1302c8fb52cbc74d1cfa"
